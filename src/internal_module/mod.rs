060d746f325f767b7c6774ad95ba31ee3717b245
Sam Ginzburg <ginzburg.sam@gmail.com>
2022-09-27 13:29:29 -0400

    more pruning of libs

--- src/internal_module/mod.rs
+++ src/internal_module/mod.rs
@@ -1,8 +1,8 @@
 pub mod core;
 pub mod encoding;
-#[cfg(feature = "img")]
-pub mod img_module;
-#[cfg(feature = "tensorflow")]
-pub mod tensorflow_module;
+//#[cfg(feature = "img")]
+//pub mod img_module;
+//#[cfg(feature = "tensorflow")]
+//pub mod tensorflow_module;
 //pub mod wasi_net_module;
 pub mod os;
